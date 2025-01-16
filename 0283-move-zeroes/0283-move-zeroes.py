341b2e311b76e2b035341887cb46cb91fbd1a291
mridul1592 <mridulsharma1592@gmail.com>
2025-01-16 19:04:03 +0530

    Time: 7 ms (63.95%), Space: 13.7 MB (5.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         lastNonZeroAt = 0
-        zero_count = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[lastNonZeroAt] = nums[i]
                 lastNonZeroAt += 1
-            elif nums[i] == 0:
-                zero_count += 1 
 
         for i in range(lastNonZeroAt, len(nums)):
             nums[i] = 0

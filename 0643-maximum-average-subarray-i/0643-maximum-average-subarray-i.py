0c48fb145ebedf7c4e3248de1a11ad061cf65ca2
mridul1592 <mridulsharma1592@gmail.com>
2025-01-18 01:28:25 +0530

    Time: 36 ms (97.5%), Space: 27.2 MB (52.02%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -11,17 +11,8 @@ class Solution:
             max_sum = sum(nums[0:k])
             s = max_sum
             print(max_sum)
-            #sum_arr.append(max_sum))
             for i in range(k, n):
                 s = s - nums[i-k] + nums[i]
-                print("num i:",nums[i])
-                print("s",s)
-                #s = sum(nums[(i-k):(i)])
                 if s > max_sum:
                     max_sum = s
-                #arrSum = sum_arr[i-k] - nums[i-k] + nums[i]
-                #sum_arr.append(arrSum)
-
-        #return max(sum_arr)/k
-        print(max_sum)
         return float(max_sum)/float(k)
